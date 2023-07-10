db049809cc0fb035fa8f9dc8b7270f73d1101d16
imranBappy <imranbappy.official@gmail.com>
2023-07-10 19:57:17 +0600

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ function App() {
         <>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home/>} />
-
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </Router>
         </>);
